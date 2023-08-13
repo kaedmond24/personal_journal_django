0285de9502a74c48cf6773c16dd620d53183600e
Kedmond24 <Kedmond24@live.com>
2023-08-12 21:46:04 -0400

    UPDATE files with auto updates applied

--- my-journal/entries/urls.py
+++ my-journal/entries/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
     path(
         "entry/<int:pk>", views.EntryDetailView.as_view(), name="entry-detail"
     ),
+    path(
+        "create", views.EntryCreateView.as_view(), name="entry-create"
+    ),
+    path(
+        "entry/<int:pk>/update", views.EntryUpdateView.as_view(), name="entry-update"
+    ),
+    path(
+        "entry/<int:pk>/delete", views.EntryDeleteView.as_view(), name="entry-delete"
+    ),
 ]
--- my-journal/entries/views.py
+++ my-journal/entries/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from .models import Entry
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
 # Create your views here.
+
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-date_created")
+
+
+class EntryDetailView(DetailView):
+    model = Entry
+
+
+class EntryCreateView(CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+
+
+class EntryUpdateView(UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+
+    def get_success_url(self) -> str:
+        return reverse_lazy(
+            "entry-detail",
+            kwargs={"pk": self.entry.id}
+        )
+
+
+class EntryDeleteView(DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
