1599fa58448054c5cc4aad00f8ed82e105cabf3d
Kedmond24 <Kedmond24@live.com>
2023-08-13 08:32:20 -0400

    FIX incorrect object reference

--- my-journal/entries/views.py
+++ my-journal/entries/views.py
@@ -43,7 +43,7 @@ class EntryUpdateView(LockedView, SuccessMessageMixin, UpdateView):
     def get_success_url(self) -> str:
         return reverse_lazy(
             "entry-detail",
-            kwargs={"pk": self.entry.id}
+            kwargs={"pk": self.object.id}
         )
 
 
